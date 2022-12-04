0407214054d1b57cb7f1ac99ed74e2ef2644330c
lesmeral <lilianapaolaesmeral@gmail.com>
2022-12-03 21:38:33 -0500

    Sixth commit backend

--- src/environments/src/index.js
+++ src/environments/src/index.js
@@ -1,28 +1,30 @@
 const express = require('express')
+const cors = require('cors')
 const { default: mongoose } = require('mongoose')
 require("dotenv").config()
 const userRoutes = require("./routes/user.js")
 const doctorRoutes = require("./routes/doctor.js")
 const petRoutes = require("./routes/pet.js")
 const appointmentRoutes = require("./routes/appointment.js")
-const cors = require("cors")
+
 
 const app = express()
 const port = process.env.PORT || 9000
 
-const corsOptions = { origin: '*' };
 
 //middlewares
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*'
+}))
 app.use(express.json())
-app.use('/user',userRoutes)
-app.use('/doctor',doctorRoutes)
-app.use('/pet',petRoutes)
-app.use('/appointment',appointmentRoutes)
+app.use('/user', userRoutes)
+app.use('/doctor', doctorRoutes)
+app.use('/pet', petRoutes)
+app.use('/appointment', appointmentRoutes)
 
 //Routes
 app.get('/', (req, res) => {
-res.send("Welcome to Hospital API")
+  res.send("Welcome to Hospital API")
 })
 
 mongoose
